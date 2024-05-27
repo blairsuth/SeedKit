ca4f852a1be853fb5fdb90284e850ec15fe66c5e
Blair Sutherland <blair@blairsutherland.com>
2024-05-27 10:52:21 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # SSH credentials
 HOST='box2302.bluehost.com'
 USER='guitarf4'
-TARGETFOLDER='/home/guitarf4/public_html/seedkit'
+TARGETFOLDER='/home4/guitarf4/public_html/seedkit'
 
 # Commands to upload files
 rsync -avz --delete ./dist/ $USER@$HOST:$TARGETFOLDER
