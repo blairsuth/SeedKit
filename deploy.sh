cd821aeac2457a7f295612d82f49e5cfa2132bb2
swdapps <sutherlandwebdesign@gmail.com>
2024-05-27 11:03:40 -0600

    test

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # SSH credentials
 HOST='box2302.bluehost.com'
 USER='guitarf4'
-TARGETFOLDER='/home4/guitarf4/public_html/seedkit'
+TARGETFOLDER='/home4/guitarf4/public_html/seedkit.happyhealthylivingllc.com'
 
 # Commands to upload files
 rsync -avz --delete ./dist/ $USER@$HOST:$TARGETFOLDER
