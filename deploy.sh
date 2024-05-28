08fb1a0e896b3e45544c988a1668b3c5bebf0f00
swdapps <sutherlandwebdesign@gmail.com>
2024-05-27 18:40:14 -0600

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # SSH credentials
-HOST='box2302.bluehost.com'
-USER='guitarf4@50.87.181.34'
+HOST='50.87.181.34'
+USER='guitarf4'
 TARGETFOLDER='/home4/guitarf4/public_html/seedkit.happyhealthylivingllc.com'
 
 # Commands to upload files
